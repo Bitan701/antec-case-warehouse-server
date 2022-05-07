8f09c0d7a286aa174883f514b1f9be1d092f164a
bitan701 <bitan701@gmail.com>
2022-05-07 15:47:15 +0600

    update and post

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 
 const app = express()
 
@@ -10,19 +10,62 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const uri =
-	'mongodb+srv://dbadmin:<password>@cluster0.vpxsj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vpxsj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const client = new MongoClient(uri, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	serverApi: ServerApiVersion.v1,
 })
-client.connect((err) => {
-	const collection = client.db('test').collection('devices')
-	console.log('casedb connected')
-	// perform actions on the collection object
-	client.close()
-})
+async function run() {
+	try {
+		await client.connect()
+		const productCollection = client.db('project-data').collection('casedb')
+
+		//get all products
+		app.get('/products', async (req, res) => {
+			console.log('query', req.query)
+
+			const query = {}
+			const cursor = productCollection.find(query)
+			const products = await cursor.toArray()
+
+			res.send(products)
+		})
+
+		// Get number of products
+		app.get('/productCount', async (req, res) => {
+			const count = await productCollection.estimatedDocumentCount()
+			res.send({ count })
+		})
+
+		//see single product with id
+		app.get('/products/:id', async (req, res) => {
+			const id = req.params.id
+			const query = { _id: ObjectId(id) }
+			const product = await productCollection.findOne(query)
+			res.send({ product })
+		})
+
+		// post new product
+		app.post('/products', async (req, res) => {
+			const newCase = req.body
+			const result = await productCollection.insertOne(newCase)
+			res.send(result)
+		})
+
+		//update a product quantity
+		// app.get('/products/:id', async (req, res) => {
+		// 	const id = req.params.id
+		// 	const query = { _id: ObjectId(id) }
+		// 	const update =
+		// 	const result = await serviceCollection.deleteOne(query)
+		// 	res.send(result)
+
+		// })
+	} finally {
+	}
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
 	res.send('running case server')
